5451b2f5c55f5c1307a3c6f0120e502a521c584b
LukeTheDank <lukecardozaisawesome@gmail.com>
2020-09-15 08:54:25 -0400

    Create egg.js

---
+++ extra/a/egg.js
@@ -0,0 +1,19 @@
+var egg=`
+    <a href="">https://dayoshiguy.github.io/tools-for-school/extra/a/boi.html<\a>
+    <a href="">https://dayoshiguy.github.io/tools-for-school/extra/a/btd.html<\a>
+    `;
+      document.addEventListener('keydown', function(e) {
+        if (e.key === 'c') {
+
+          document.addEventListener('keydown', function(e) {
+            if (e.key === 'd') {
+
+              document.addEventListener('keydown', function(e) {
+                if (e.key === '1') {
+                  document.write(egg);
+                }
+              })
+            }
+          })
+        }
+      })
